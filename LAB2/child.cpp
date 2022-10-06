7731c80921bf77ffe10167494165fbbbbce3ee88
Vladislav Bobovoz <108393356+vbobovoz@users.noreply.github.com>
2022-10-06 20:15:03 +0300

    Add files via upload

---
+++ LAB2/child.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Child created." << endl;
+	
+	if (argc > 1) {
+		int size = atoi(argv[1]);
+		cout << "  Size of array: " << size << endl;
+		int* arr = new int[size];
+
+		for (int i = 2, j = 0; i < argc; ++i) {
+			arr[j] = atoi(argv[i]);
+			++j;
+		}
+		
+		for (int i = 0; i < size - 1; ++i) {
+			for (int j = 0; j < size - i - 1; ++j) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+
+		cout << "  Array after bubble sort: ";
+		for (int i = 0; i < size; ++i) {
+			cout << arr[i] << " ";
+		}
+
+		delete[] arr;
+	}
+	else {
+		cout << "Error!" << endl << endl;
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ LAB2/parent.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include <tchar.h>
+using namespace std;
+
+int main() {
+	int size;
+	cout << "Enter size of array: ";
+	cin >> size;
+	cout << "Enter " << size << " elements of array: ";
+	int* array = new int[size];
+	for (int i = 0; i < size; ++i) {
+		cin >> array[i];
+	}
+
+	wstring cmd_info = L"\"child.exe\" ";
+	cmd_info += to_wstring(size);
+	cmd_info += L" ";
+	for (int i = 0; i < size; i++) {
+		cmd_info += to_wstring(array[i]);
+		if (i != size - 1) {
+			cmd_info += L" ";
+		}
+	}
+
+	wchar_t* stemp = new wchar_t[cmd_info.length()];
+	wcscpy_s(stemp, cmd_info.length() + 1, cmd_info.c_str());
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION piApp;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	si.dwFlags = STARTF_USECOUNTCHARS;
+	si.dwYCountChars = 1000;
+
+	if (!CreateProcess(nullptr, stemp, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &si, &piApp)) {
+		cout << "New process is not created" << endl;
+		return 0;
+	}
+
+	cout << "New process is created" << endl;
+	WaitForSingleObject(piApp.hProcess, INFINITE);
+	CloseHandle(piApp.hThread);
+	CloseHandle(piApp.hProcess);
+	delete[]array;
+	return 0;
+}
