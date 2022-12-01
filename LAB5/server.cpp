d2cafc1895afd8a51e143c24c6b07cf106fa30b0
Vladislav Bobovoz <108393356+vbobovoz@users.noreply.github.com>
2022-12-01 10:30:33 +0300

    Add files via upload

---
+++ LAB5/server.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void main(int argc, char** argv) {
+	srand(time(0));
+	int size;
+	cout << "-Server-\n Enter array size: ";
+	cin >> size;
+	double* arr = new double[size];
+	for (int i = 0; i < size; ++i) {
+		arr[i] = rand() % 100 + 1;
+	}
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	HANDLE hWritePipe, hReadPipe, hEvent;
+	SECURITY_ATTRIBUTES sa;
+
+	hEvent = CreateEvent(nullptr, FALSE, FALSE, L"Event");
+	if (!hEvent) {
+		cout << "-Server-\n Event was not created";
+		return;
+	}
+
+	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+	sa.lpSecurityDescriptor = nullptr;
+	sa.bInheritHandle = TRUE;
+
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, 0)) {
+		cout << "-Server-\n Pipe was not created";
+		return;
+	}
+
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	wchar_t* wstr = new wchar_t[80];
+	wsprintf(wstr, L"\"sum.exe\" %d %d %d", (int)hWritePipe, (int)hReadPipe, (int)size);
+
+	if (!CreateProcess(nullptr, wstr, nullptr, nullptr, TRUE, CREATE_NEW_CONSOLE, FALSE, FALSE, &si, &pi)) {
+		cout << "-Server-\n sum creation failed";
+		return;
+	}
+
+	double n;
+	cout << "-Server-\n Enter n: ";
+	cin >> n;
+	double* info = new double[size + 2];
+	info[0] = n; info[1] = (double)size;
+	for (int i = 2; i < size + 2; ++i) {
+		info[i] = arr[i - 2];
+	}
+
+	DWORD dwBytesWritten;
+	if (!WriteFile(hWritePipe, info, sizeof(double) * (size + 2), &dwBytesWritten, nullptr)) {
+		cout << "-Server-\n info was not written";
+		return;
+	}
+
+	WaitForSingleObject(hEvent, INFINITE);
+
+	DWORD dwBytesRead;
+	if (!ReadFile(hReadPipe, arr, sizeof(double) * (size + 2), &dwBytesRead, nullptr)) {
+		cout << "-Server-\n arr was not read";
+		return;
+	}
+
+	cout << "-Server-\n";
+	cout << " n: " << info[0] << "  size: " << info[1] << "\n";
+	cout << " Random numbers: ";
+	for (int i = 0; i < info[1]; ++i) {
+		cout << info[i + 2] << " ";
+	} cout << "\n\n";
+
+	cout << "-Sum-\n";
+	cout << " sum: " << arr[0] << "\n";
+	cout << " elements: ";
+	for (int i = 2; i < arr[1] + 2; ++i) {
+		cout << arr[i] << " ";
+	} cout << "\n";
+
+	CloseHandle(hWritePipe);
+	CloseHandle(hReadPipe);
+	CloseHandle(hEvent);
+}
---
+++ LAB5/sum.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+using namespace std;
+
+void main(int argc, char** argv) {
+	HANDLE hWritePipe = (HANDLE)atoi(argv[1]);
+	HANDLE hReadPipe = (HANDLE)atoi(argv[2]);
+	int size = atoi(argv[3]);
+	cout << size << "\n";
+	HANDLE hEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"Event");
+	if (!hEvent) {
+		cout << "-Sum-\n event was not read";
+	}
+
+	double* info = new double[size + 2];
+
+	DWORD dwBytesRead;
+	if (!ReadFile(hReadPipe, info, sizeof(double) * (size + 2), &dwBytesRead, NULL)) {
+		cout << "-Sum-\n info was not read";
+	}
+
+	double* arr = new double[info[1]];
+	int n = (int)info[0];
+	
+	vector<double> v;
+	double total = 0, count = 0;
+	cout << "-Sum-\n";
+	for (int i = 2; i < size + 2; ++i) {
+		if (info[i] > n) {
+			cout << info[i] << " ";
+			v.push_back(info[i] * info[i]);
+			total += info[i] * info[i];
+			count++;
+		}
+	}
+
+	double* res = new double[v.size() + 1];
+	res[0] = total;
+	res[1] = count;
+	for (int i = 2; i < v.size() + 2; ++i) {
+		res[i] = v[i - 2];
+	}
+
+	DWORD dwBytesWritten;
+	if (!WriteFile(hWritePipe, res, sizeof(double) * (v.size() + 2), &dwBytesWritten, NULL)) {
+		cout << "-Sum-\n res was not written";
+	}
+
+	SetEvent(hEvent);
+	CloseHandle(hWritePipe);
+	CloseHandle(hReadPipe);
+	CloseHandle(hEvent);
+
+	while(_getch() != '0'){}
+}
